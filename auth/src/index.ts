03fb1d4dc2c29d71ddfbbb2feb0988d4c4c70bd5
Csornyei Mate <mate@csornyei.com>
2021-03-13 22:19:08 +0100

    Basic ingress setup

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,10 @@ import { json } from 'express';
 const app = express();
 app.use(json());
 
+app.get('/api/users/currentuser', (req, res) => {
+    res.send('Hi there!')
+})
+
 app.listen(3000, () => {
     console.log('Auth Service is listening on port 3000');
 })
