ace66cc3b3edcaff98a60fe82a39f483aeb6530a
Csornyei Mate <mate@csornyei.com>
2021-03-17 10:00:46 +0100

    Getting current user

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,10 +1,21 @@
-const Index = () => {
+import buildClient from '../api/build-client';
+
+const LandingPage = ({ currentUser }) => {
     return (
         <div>
-            <h1>My Landing page</h1>
-            <button className="btn btn-primary">Sign up!</button>
+            { currentUser ? <h1>You are signed in</h1> : <h1>You are not signed in</h1> }
+            { !currentUser &&
+                <a className="btn btn-primary" href="/auth/signup">Sign up</a>
+            }
         </div>
     )
 };
 
-export default Index;
+LandingPage.getInitialProps = async (context) => {
+    const axios = buildClient(context);
+    const { data } = await axios.get('/api/users/currentuser');
+
+    return data;
+}
+
+export default LandingPage;
