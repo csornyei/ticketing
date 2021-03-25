2a6467fb101ca8668cc3b41488d2b050254fb5df
matecsornyei <mate@csornyei.com>
2021-03-25 12:13:21 +0100

    Basic route handlers

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -4,6 +4,11 @@ import { json } from 'express';
 import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError, currentUser } from '@csornyei-ticketing/common';
 
+import { indexOrderRouter } from './routes/index';
+import { showOrderRouter } from './routes/show';
+import { newOrderRouter } from './routes/new';
+import { deleteOrderRouter } from './routes/delete';
+
 const app = express();
 // express is aware it's behind a proxy and trust it
 app.set('trust proxy', true);
@@ -13,6 +18,11 @@ app.use(cookieSession({
     secure: process.env.NODE_ENV !== 'test'
 }));
 app.use(currentUser);
+
+app.use(indexOrderRouter);
+app.use(showOrderRouter);
+app.use(newOrderRouter);
+app.use(deleteOrderRouter);
 
 app.all('*', async () => {
     throw new NotFoundError();
