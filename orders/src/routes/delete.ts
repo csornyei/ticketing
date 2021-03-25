e390f9a92a913dc5afb2c618467dcdfdfdb00e5b
matecsornyei <mate@csornyei.com>
2021-03-25 15:48:58 +0100

    Cancelling an order

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,25 @@
+import { NotAuthorizedError, NotFoundError, requireAuth } from '@csornyei-ticketing/common';
 import { Router, Request, Response } from 'express';
+
+import { Order, OrderStatus } from '../models/orders';
 
 const router = Router();
 
-router.delete('/api/orders/:id', async (req: Request, res: Response) => {
-    res.send({});
-});
+router.delete('/api/orders/:orderId',
+    requireAuth,
+    async (req: Request, res: Response) => {
+        const order = await Order.findById(req.params.orderId).populate('ticket');
+
+        if (!order) {
+            throw new NotFoundError();
+        }
+        if (order.userId !== req.currentUser!.id) {
+            throw new NotAuthorizedError();
+        }
+        order.status = OrderStatus.Cancelled;
+        await order.save();
+
+        res.status(204).send(order);
+    });
 
 export { router as deleteOrderRouter };
