f5dea6e9ac13a84fa6a2e1834e35c573bb10bfd2
matecsornyei <mate@csornyei.com>
2021-03-25 12:07:38 +0100

    Basic orders service

---
+++ orders/src/app.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'express';
+import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError, currentUser } from '@csornyei-ticketing/common';
+
+const app = express();
+// express is aware it's behind a proxy and trust it
+app.set('trust proxy', true);
+app.use(json());
+app.use(cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test'
+}));
+app.use(currentUser);
+
+app.all('*', async () => {
+    throw new NotFoundError();
+})
+app.use(errorHandler);
+
+export default app;
