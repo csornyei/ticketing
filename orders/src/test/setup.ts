4f73b72425a292075b1bac9367dd8e56da644941
matecsornyei <mate@csornyei.com>
2021-03-25 14:52:26 +0100

    test for creating order

---
+++ orders/src/test/setup.ts
@@ -0,0 +1,57 @@
+import jwt from 'jsonwebtoken';
+import mongoose from 'mongoose';
+import { MongoMemoryServer } from "mongodb-memory-server";
+
+declare global {
+    namespace NodeJS {
+        interface Global {
+            signin(): string[];
+        }
+    }
+}
+
+jest.mock('../nats-wrapper.ts');
+
+let mongo: MongoMemoryServer;
+beforeAll(async () => {
+    process.env.JWT_SECRET_KEY = "test";
+
+    mongo = new MongoMemoryServer();
+    const mongoUri = await mongo.getUri();
+
+    await mongoose.connect(mongoUri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+});
+
+beforeEach(async () => {
+    jest.clearAllMocks();
+    const collections = await mongoose.connection.db.collections();
+
+    for (let collection of collections) {
+        await collection.deleteMany({});
+    }
+});
+
+afterAll(async () => {
+    await mongo.stop();
+    await mongoose.connection.close();
+});
+
+global.signin = () => {
+    // Build JWT payload
+    const id = mongoose.Types.ObjectId().toHexString()
+    const JWT_PAYLOAD = {
+        id,
+        email: 'test@test.com'
+    };
+    // Create a JWT
+    const token = jwt.sign(JWT_PAYLOAD, process.env.JWT_SECRET_KEY!);
+    // Build session Object. { jwt: MY_JWT }
+    const session = { jwt: token };
+    // Turn the session to base64 encoded JSON
+    const base64 = Buffer.from(JSON.stringify(session)).toString('base64');
+    // Return a string thats the cookie with the encoded data
+    return [`express:sess=${base64}`];
+};
