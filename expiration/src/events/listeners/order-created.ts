dd19f0ea5ad59fffd5f5d07458ce322b0cfe28be
matecsornyei <mate@csornyei.com>
2021-03-28 20:56:20 +0200

    Delaying job

--- expiration/src/events/listeners/order-created.ts
+++ expiration/src/events/listeners/order-created.ts
@@ -8,8 +8,11 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
     readonly queueGroupName = queueGroupName;
 
     async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+        const delay = new Date(data.expiresAt).getTime() - new Date().getTime()
         await expirationQueue.add({
             orderId: data.id
+        }, {
+            delay
         });
 
         msg.ack();
